9152a9691081d79c9f64522157453de1f4249ce5
Guilherme Olegario <guigui300200@gmail.com>
2025-08-12 14:40:43 -0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+usuario = input("digite o usuario: ")
+senha = input("digite a senha: ")
+
+if usuario == "Guilherme" and senha == "1234":
+ print("Bem-vindo Guilherme 🔓")
+
+else:
+    print("Usuario n encontrado 🔐. Tesnte Novamente")
